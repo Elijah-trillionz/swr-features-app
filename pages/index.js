264a84d0e15988aa88f95a66b698f797e942a917
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2023-07-18 05:43:02 +0100

    changed home page

--- pages/index.js
+++ pages/index.js
@@ -1,110 +1,11 @@
-import { useEffect, useState } from 'react';
-import useSWR from 'swr';
-
-const Posts = () => {
-  const {
-    data: posts,
-    isLoading,
-    error: isError,
-    mutate,
-  } = useSWR('http://localhost:5000/api/posts', (url) =>
-    fetch(url).then((res) => res.json())
-  );
-
-  const [displayForm, setDisplayForm] = useState(false);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-
-  if (isError) return <div>Error loading posts</div>;
-
-  const addPost = async () => {
-    if (!postTitle || !postBody) return;
-
-    const addNewPost = async () => {
-      const res = await fetch('http://localhost:5000/api/post/new', {
-        method: 'post',
-        headers: {
-          'content-type': 'application/json',
-          accept: 'application/json',
-        },
-        body: JSON.stringify({
-          title: postTitle,
-          body: postBody,
-          id: posts.length + 1,
-        }),
-      });
-
-      return await res.json();
-    };
-    mutate(addNewPost, {
-      populateCache: (newPost, posts) => {
-        return [...posts, newPost];
-      },
-      revalidate: false,
-    });
-  };
-
+export default function Home() {
   return (
-    <>
-      <header className='p-7 text-center bg-primary shadow-md'>
-        <h1 className='text-secondary font-bold text-xl'>Posts App</h1>
-      </header>
-      <main className='max-w-[1100px] mx-auto mt-10 pb-10 px-4'>
-        {isLoading ? (
-          <p className='text-secondary'>Loading posts</p>
-        ) : (
-          <>
-            <ul className='grid grid-cols-3 gap-x-3 gap-y-3'>
-              {posts.map((post, index) => (
-                <li
-                  className='bg-primary p-10 rounded-lg shadow-md'
-                  key={post.id}
-                >
-                  <span className='block mb-4 font-bold text-lg text-secondary capitalize'>
-                    {post.title}
-                  </span>
-                  <span className='text-secondary'>{post.body}</span>
-                </li>
-              ))}
-              <button
-                className='bg-primary p-10 rounded-lg shadow-md'
-                onClick={() => setDisplayForm(true)}
-              >
-                <span className='block mb-4 font-bold text-lg text-secondary'>
-                  add new +
-                </span>
-              </button>
-            </ul>
-            {displayForm && (
-              <>
-                <input
-                  type='text'
-                  onChange={(e) => setPostTitle(e.target.value)}
-                  placeholder='Enter post title'
-                  name='post-title'
-                  className='block'
-                />
-                <input
-                  type='text'
-                  onChange={(e) => setPostBody(e.target.value)}
-                  placeholder='Enter post body'
-                  name='post-body'
-                  className='block'
-                />
-                <button
-                  type='submit'
-                  className='block bg-primary p-5 text-secondary rounded-lg shadow-md'
-                  onClick={addPost}
-                >
-                  Add post
-                </button>
-              </>
-            )}
-          </>
-        )}
-      </main>
-    </>
+    <div>
+      <h2>
+        This is a fully functional and interactive app showing examples of using
+        SWR features
+      </h2>
+      <p>Built by Elijah and styled with Tailwind</p>
+    </div>
   );
-};
-
-export default Posts;
+}
